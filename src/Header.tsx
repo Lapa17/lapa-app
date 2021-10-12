4b571e8090240b5e41e47f418b0c40b1df7fb556
Pavel Laparevich <laparevichpavel@gmail.com>
2021-10-12 23:17:12 +0300

    add components

---
+++ src/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <li>Logo</li>
+        <li>Catalog</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>JSX</li>
+        <li>React</li>
+        <li>TS</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Technologies;
