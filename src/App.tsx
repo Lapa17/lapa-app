ce088c648a4b5ba18a0aa9cbecf7723bc5916e97
Pavel Laparevich <laparevichpavel@gmail.com>
2021-10-13 20:06:19 +0300

    create new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Footer from './Footer';
-import Technologies from './Technologies';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+
 
 const App = () => {
   return (
-    <div>
+    <div className='app-wrapper'>
       <Header />
-      <Technologies />
-      <Footer />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+    <div>
+      <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My post
+      <div>
+        New post
+      </div>
+      <div>
+        <div>Post1</div>
+        <div>Post2</div>
+        <div>Post3</div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
