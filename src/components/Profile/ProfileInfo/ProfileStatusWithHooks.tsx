2e608ef3bf0cee65d774cd60a75c88462ffc6243
Pavel Laparevich <laparevichpavel@gmail.com>
2022-03-27 08:42:15 +0300

    added useEffect to Profile satatus component

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 type ProfileStatusType = {
     status: string
@@ -8,6 +8,10 @@ type ProfileStatusType = {
 export const ProfileStatusWithHooks = (props: ProfileStatusType) => {
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+
+    useEffect(()=>{
+        setStatus(props.status)
+    }, [props.status])
 
     const onEditMode = () => {
         setEditMode(true)
--- src/utilits/selectors/user-selector.ts
+++ src/utilits/selectors/user-selector.ts
@@ -1,8 +1,13 @@
 import {AppRootStateType} from "../../redux/redux-store";
+import {createSelector} from 'reselect'
 
 export const getUser = (state:AppRootStateType)=> {
     return state.users.users
 }
+
+// export const getUserSelector = createSelector (getUser, (users)=> { //example of selector with reselect
+//     return users.filter((u: any)=> u)
+// })
 
 export const getPageSize = (state:AppRootStateType)=> {
     return state.users.pageSize
