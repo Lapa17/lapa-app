eb601d4af8b69773c3aea2c1ff44594590af3b96
Lapa17 <laparevichpavel@gmail.com>
2022-04-05 15:49:14 +0300

    added ant design paginator

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -1,6 +1,7 @@
 import s from './Paginator.module.css'
 import {useEffect, useState} from "react";
 import cn from 'classnames';
+import {Pagination} from "antd";
 
 type PaginatorPropsType = {
     totalItemsCounter: number
@@ -20,6 +21,7 @@ export const Paginator = ({
                               portionSize = 10
                           }: PaginatorPropsType) => {
 
+
     const pagesCount = Math.ceil(totalItemsCounter / pageSize)
     const pages = []
     for (let i = 1; i <= pagesCount; i++) {
@@ -35,13 +37,31 @@ export const Paginator = ({
         setPortionNumber(Math.ceil(currentPage / portionSize))
     }, [currentPage, portionSize])
 
+    function onShowSizeChange(current:number, pageSize:number) {
+        console.log(current, pageSize);
+    }
+
+    const onChangePage = (page: number, pageSize: number) => {
+        onPageClick(page)
+    }
+
     return (
         <div>
-            {portionNumber > 1 && <button onClick={()=> setPortionNumber(portionNumber -1 )}>Prev</button>}
-                {pages
-                    .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
-                    .map(p => <span className={currentPage === p ? cn(s.selected, s.page) : s.page}
-                              onClick={() => onPageClick(p)}>{p}</span> )}
-            {portionNumber < portionCount && <button onClick={()=> setPortionNumber(portionNumber +1 )}>Next</button>}
+        {/*<div>*/}
+        {/*    {portionNumber > 1 && <button onClick={()=> setPortionNumber(portionNumber -1 )}>Prev</button>}*/}
+        {/*        {pages*/}
+        {/*            .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)*/}
+        {/*            .map(p => <span className={currentPage === p ? cn(s.selected, s.page) : s.page}*/}
+        {/*                      onClick={() => onPageClick(p)}>{p}</span> )}*/}
+        {/*    {portionNumber < portionCount && <button onClick={()=> setPortionNumber(portionNumber +1 )}>Next</button>}*/}
+        {/*</div>*/}
+            <Pagination
+                showSizeChanger={false}
+                onShowSizeChange={onShowSizeChange}
+                defaultCurrent={1}
+                current={currentPage}
+                total={pagesCount}
+                onChange={onChangePage}
+            />
         </div>)
 }
